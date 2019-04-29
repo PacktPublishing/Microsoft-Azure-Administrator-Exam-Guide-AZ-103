e672128d63552dbbaba8eb25d52488194d59cb95
Sjoukje Zaal <szaal@outlook.com>
2019-04-29 13:14:13 +0200

    Create CreateVMsAvailabilitySet.ps1

---
+++ Chapter 7/CreateVMsAvailabilitySet.ps1
@@ -0,0 +1,35 @@
+﻿#First, we need to login to the Azure account:
+Login-AzureRmAccount
+
+#If necessary, select the right subscription:
+Select-AzureRmSubscription -SubscriptionId "********-****-****-****-***********"
+
+#Create a resource group for the Availability Set:
+New-AzResourceGroup -Name PacktVMResourceGroup -Location EastUS
+
+#Create an availability set for the VMs:
+New-AzAvailabilitySet `
+   -Location "EastUS" `
+   -Name "PacktVMAvailabilitySet" `
+   -ResourceGroupName PacktVMResourceGroup `
+   -Sku aligned `
+   -PlatformFaultDomainCount 2 `
+   -PlatformUpdateDomainCount 2
+
+#Set the administrator credentials for the VMs:
+$cred = Get-Credential
+
+#Create the two virtual machines inside the Availability Set:
+for ($i=1; $i -le 2; $i++)
+{
+    New-AzVm `
+        -ResourceGroupName PacktVMResourceGroup `
+        -Name "PacktVM$i" `
+        -Location "East US" `
+        -VirtualNetworkName "PacktVnet" `
+        -SubnetName "PacktSubnet" `
+        -SecurityGroupName "PacktNetworkSecurityGroup" `
+        -PublicIpAddressName "PacktPublicIpAddress$i" `
+        -AvailabilitySetName "PacktVMAvailabilitySet" `
+        -Credential $cred
+}
