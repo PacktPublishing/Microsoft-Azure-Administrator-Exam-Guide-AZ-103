ac835d44ba22b0dd671cd7c3e73eedf9c19002f6
Sjoukje Zaal <szaal@outlook.com>
2019-05-09 08:10:58 +0200

    Added ch13

---
+++ Chapter 13/Install_IIS.ps1
@@ -0,0 +1,8 @@
+﻿# Install IIS
+  Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Remove default htm file
+ remove-item  C:\inetpub\wwwroot\iisstart.htm
+
+#Add custom htm file
+ Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Hello World from " + $env:computername)
